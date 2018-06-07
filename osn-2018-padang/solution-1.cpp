19ea75d4c875e35c46e181bafca814b59f9564bf
Muhammad Ayaz Dzulfikar <muhammad.ayaz97@gmail.com>
2018-06-07 12:56:26 +0700

    Add solution-1 for osn-2018-padang

---
+++ osn-2018-padang/solution-1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const string POSSIBLE = "MUNGKIN";
+const string IMPOSSIBLE = "TIDAK MUNGKIN";
+const string OUT_OF_BOUND = "DI LUAR BATAS";
+
+int main() {
+  cout << POSSIBLE << "\n";
+  cout << "1100\n";
+  return 0;
+}
