1a3978a014ec328140675642e7ba8375724844ba
Febriananda Wida Pramudita <p.phebri@yahoo.com>
2018-06-14 21:56:33 +0700

    add solution-2 for osn-2018-pajel

---
+++ osn-2018-pajel/solution-2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char** argv) {
+	printf("BBBB--M---\n");
+	printf("B-M------M\n");
+	printf("B---M-----\n");
+	printf("-M--MM---B\n");
+	printf("-M-----B--\n");
+	
+	printf("------BB--\n");
+	printf("----M----M\n");
+	printf("-M--------\n");
+	printf("----------\n");
+	printf("-----MMM--\n");
+}
