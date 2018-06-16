da38bd1ba2c27dbe98583908aad231d22238da4c
maxhaibara <maxhaibara97@gmail.com>
2018-06-16 21:40:03 +0700

    update solution 8 for osn-2018-fpb

--- osn-2018-fpb/solution-8.cpp
+++ osn-2018-fpb/solution-8.cpp
@@ -22,30 +22,12 @@ ll f(int cur,int sisa,bool ambil) {
 	return ret=(f(cur-1,sisa-1,true)+f(cur-1,sisa,false))%MOD;
 }
 
-inline void prework() {
-	memset(DP,-1,sizeof DP); memset(PS,0,sizeof PS);
-	for (int i=0;i<k;i++) {
-		PS[0][i]=f(0,i,false);
-		for (int j=1;j<n;j++) PS[j][i]=(PS[j-1][i]+f(j,i,false))%MOD;
-	}
-}
-
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	string header; cin>>header; 
 	cin>>n>>k; 
 	for (int i=0;i<n;i++) cin>>A[i];
-	prework();
-	ll ans=0;
-	for (int i=0;i<n;i++) {
-		for (int j=0;j<k;j++) {
-			ll query;
-			if (k-1-j==0) query=1;
-			else if (i==n-1) query=0;
-			else query=f(n-i-1,k-1-j,false);
-			ans+=kali(kali(A[0],PS[i][j]),query); ans%=MOD;
-		}
-	}
-	cout<<ans<<endl;
+	memset(DP,-1,sizeof DP);
+	cout<<kali(f(n,k,0),kali(k,A[0]))<<endl;
 	return 0;
 }
