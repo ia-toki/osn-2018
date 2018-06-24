98a876068fc1021214189838076bda0bbd7af757
raisfathin <rais.fathin38@gmail.com>
2018-06-24 21:07:27 +0800

    Fix solution

--- osn-2018-ojek/solution.cpp
+++ osn-2018-ojek/solution.cpp
@@ -22,7 +22,7 @@ template <typename T> using V = vector<T>;
 template <typename T> using VV = vector<V<T>>;
 template <typename T> using VVV = vector<VV<T>>;
 
-const int N = 205;
+const int N = 105;
 
 struct Edge {
   int to, dist;
@@ -58,15 +58,16 @@ struct PQEntry {
 
 int n, m;
 int Cg, Mg, Co, Mo;
-ll dist[N][N][2]; // dist, excess, temporary
+ll dist[N][2 * N][2]; // dist, excess, temporary
 vector<Edge> blockedEdge[N];
 vector<Edge> unblockedEdge[N];
 
 template<typename PQ>
-void relax(PQ &pq, int to, ll value, int excess) {
-  if (dist[to][excess] > value) {
-    dist[to][excess] = value;
-    pq.push(PQEntry(to, excess, value));
+void relax(PQ &pq, int to, int excess, int temporary, ll value) {
+  assert(temporary || excess < Mo);
+  if (dist[to][excess][temporary] > value) {
+    dist[to][excess][temporary] = value;
+    pq.push(PQEntry(to, excess, temporary, value));
   }
 }
 
@@ -74,16 +75,19 @@ template<typename PQ>
 void processNodeBlocked(PQ &pq, int u, int excess, ll value, bool firstTimePop) {
   auto useExcess = [&](int to, int k) {
     if (excess >= k) {
-      relax(pq, to, value, excess - k);
+      relax(pq, to, excess - k, false, value);
     } else {
       int rem = k - excess;
-      relax(pq, to, value + 1LL * (rem + Mo - 1) / Mo * Co, (Mo - rem % Mo) % Mo);
+      ll opangCost = 1LL * (rem + Mo - 1) / Mo * Co;
+      relax(pq, to, (Mo - rem % Mo) % Mo, false, value + opangCost);
     }
   };
   auto discardExcess = [&](int to, int k) {
     for (int distG = 0; distG <= Mg && distG <= k; distG++) {
       int rem = k - distG;
-      relax(pq, to, value + 1LL * distG * Cg + 1LL * (rem + Mo - 1) / Mo * Co, (Mo - rem % Mo) % Mo);
+      ll gojekCost = 1LL * distG * Cg;
+      ll opangCost = 1LL * (rem + Mo - 1) / Mo * Co;
+      relax(pq, to, (Mo - rem % Mo) % Mo, false, value + gojekCost + opangCost);
     }
   };
   for (const Edge &e : blockedEdge[u]) {
@@ -95,29 +99,40 @@ void processNodeBlocked(PQ &pq, int u, int excess, ll value, bool firstTimePop) 
 }
 
 template<typename PQ>
-void processNodeUnblocked(PQ &pq, int u, int excess, ll value) {
-  auto useGojek = [&](int to, int k) {
-    relax(pq, to, value + 1LL * k * Cg, 0);
-  };
-  auto useOpang = [&](int to, int k) {
-    int cnt = (k + Mo - 1) / Mo;
-    assert(cnt > 0);
-    relax(pq, to, value + 1LL * cnt * Co, (Mo - k % Mo) % Mo);
-    {
-      int coveredByOpang = (cnt - 1) * Mo;
-      int coveredByGojek = k - coveredByOpang;
-      assert(coveredByOpang >= 0 && coveredByGojek > 0);
-      relax(pq, to, value + 1LL * (cnt - 1) * Co + 1LL * coveredByGojek * Cg, 0);
-    }
-  };
+void processNodeUnblockedNonTemporary(PQ &pq, int u, int excess, ll value) {
+  relax(pq, u, excess, true, value);
   for (const Edge &e : unblockedEdge[u]) {
     if (excess >= e.dist) {
-      relax(pq, e.to, value, excess - e.dist);
-      continue;
+      relax(pq, e.to, excess - e.dist, false, value);
     }
-    int rem = e.dist - excess;
-    useGojek(e.to, rem);
-    useOpang(e.to, rem);
+  }
+}
+
+template<typename PQ>
+void processNodeUnblockedTemporary(PQ &pq, int u, int excess, ll value) {
+  auto doGojekStep = [&]() {
+    if (excess + 1 >= 2 * N) {
+      return;
+    }
+    relax(pq, u, excess + 1, true, value + Cg);
+  };
+
+  auto doOpangStep = [&](int to, int k) {
+    int cnt = (k + Mo - 1) / Mo;
+    ll stepCost = min<ll>(Co, 1LL * Mo * Cg);
+    ll tripCost = cnt == 0 ? 0 : ((cnt - 1) * stepCost + Co);
+    if (k % Mo == 0) {
+      tripCost = min(tripCost, cnt * stepCost);
+      if (cnt == 0) assert(tripCost == 0);
+    }
+    relax(pq, to, (Mo - k % Mo) % Mo, false, value + tripCost);
+  };
+
+  doGojekStep();
+  for (const Edge &e : unblockedEdge[u]) {
+    if (excess <= e.dist) {
+      doOpangStep(e.to, e.dist - excess);
+    }
   }
 }
 
@@ -126,29 +141,40 @@ ll solve(int s, int t) {
 
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
-      dist[i][j] = LLONG_MAX / 2;
+      dist[i][j][0] = dist[i][j][1] = LLONG_MAX / 2;
     }
   }
 
   priority_queue<PQEntry, vector<PQEntry>, PQEntry::DefaultComparator> pq;
-  pq.push(PQEntry(s, 0, 0));
-  dist[s][0] = 0;
+  pq.push(PQEntry(s, 0, false, 0));
+  dist[s][0][0] = 0;
   while (!pq.empty()) {
     int u = pq.top().u;
     int excess = pq.top().excess;
+    int temporary = pq.top().temporary;
     ll value = pq.top().value;
     if (u == t) {
       break;
     }
     pq.pop();
-    if (dist[u][excess] != value) {
+    if (dist[u][excess][temporary] != value) {
       continue;
     }
-    processNodeBlocked(pq, u, excess, value, !popped[u]);
-    popped[u] = true;
-    processNodeUnblocked(pq, u, excess, value);
+    if (!temporary) {
+      processNodeBlocked(pq, u, excess, value, !popped[u]);
+      popped[u] = true;
+      processNodeUnblockedNonTemporary(pq, u, excess, value);
+    } else {
+      processNodeUnblockedTemporary(pq, u, excess, value);
+    }
   }
-  return *min_element(begin(dist[t]), end(dist[t]));
+
+  ll ans = LLONG_MAX;
+  for (int i = 0; i < N; i++) {
+    ans = min(ans, dist[t][i][0]);
+  }
+
+  return ans;
 }
 
 int main() {
