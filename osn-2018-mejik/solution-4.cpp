744a830e3a6e971f51a20776b4bc80d8ed0f6291
Ammar Fathin Sabili <athin2008@gmail.com>
2018-06-26 13:21:48 +0800

    Add solution-4 of mejik

---
+++ osn-2018-mejik/solution-4.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+void desc1() {
+	printf("1 20 8 9 14\n");
+	printf("2 6 7 10 13\n");
+	printf("3 22 16 11 12\n");
+	printf("4 17 21 19 18\n");
+	printf("5 25 15 23 24\n");
+}
+
+void desc2() {
+	printf("3 25 13 18 16\n");
+	printf("1 9 7 11 10\n");
+	printf("5 6 17 15 21\n");
+	printf("2 20 8 12 14\n");
+	printf("4 19 22 24 23\n");
+}
+
+void desc3() {
+	printf("1 6 11 16 21\n");
+	printf("2 7 12 17 22\n");
+	printf("3 8 13 18 23\n");
+	printf("4 9 14 19 24\n");
+	printf("5 10 15 20 25\n");
+}
+
+void sub1() {
+	printf("1 5 7 6\n");
+	printf("2 8 10 9 \n");
+	printf("3 11 13 12\n");
+	printf("4 14 16 15\n");	
+}
+
+void sub2() {
+	printf("1 6 7 8 4\n");
+	printf("2 12 13 14 9\n");
+	printf("3 15 16 17 10\n");
+	printf("5 18 19 20 11\n");
+	printf("21 23 24 25 22\n");	
+}
+
+void sub3() {
+	printf("13 22 18 27 11 20\n");	
+	printf("31 4 36 9 29 2\n");	
+	printf("12 21 14 23 16 25\n");	
+	printf("30 3 5 32 34 7\n");	
+	printf("17 26 10 19 15 24\n");	
+	printf("8 35 28 1 6 33\n");	
+}
+
+void sub4() {
+	printf("34 3 32 6 15 39 14\n"); 
+	printf("20 5 8 31 25 35 11\n");
+	printf("26 13 1 22 23 12 43\n");
+	printf("46 2 10 47 28 21 16\n");
+	printf("9 38 27 30 18 49 40\n");
+	printf("44 45 48 19 24 37 42\n");
+	printf("7 41 4 33 29 36 17\n");
+}
+
+void sub5() {
+	printf("22 11 55 39 19 10 60 45\n");
+	printf("18 46 64 29 62 7 33 50\n");
+	printf("47 53 52 4 15 35 8 5\n");
+	printf("56 9 38 31 43 44 54 6\n");
+	printf("24 37 32 26 57 13 25 61\n");
+	printf("63 30 42 20 23 2 16 27\n");
+	printf("17 36 3 12 28 51 40 14\n");
+	printf("59 58 49 48 41 34 21 1\n");
+}
+
+int main() {
+	sub4();
+}
