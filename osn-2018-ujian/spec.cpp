ffe7a7c37f8e22ab875144adbe1f81c73a011369
maxhaibara <maxhaibara97@gmail.com>
2018-06-26 17:45:09 +0700

    update constraint N<=100k, reduce tc size

--- osn-2018-ujian/spec.cpp
+++ osn-2018-ujian/spec.cpp
@@ -43,7 +43,7 @@ protected:
     void Constraints() {
 		CONS(HEADER.size()==8);
 		CONS(ValidHeader(HEADER));
-		CONS(Between(N,1,200000));
+		CONS(Between(N,1,100000));
 		CONS(Between(K,1,N));
 		CONS(Unique(P));
 		CONS(AllBetween(A,1,K));
@@ -54,7 +54,7 @@ protected:
 	}
 	
 	void Subtask1() {	//Open Subtask 1
-		Points(4);
+		Points(6);
 		CONS(HEADER[1]=='1');
 		CONS(N==5);
 		CONS(K==1);
@@ -67,7 +67,7 @@ protected:
 	}
 	
 	void Subtask2() {	//Open Subtask 2
-		Points(9);
+		Points(8);
 		CONS(HEADER[2]=='2');
 		CONS(N==9);
 		CONS(K==4);
@@ -80,32 +80,32 @@ protected:
 	}
 	
 	void Subtask3() {	//Cek ada solusi/tidak
-		Points(12);
+		Points(10);
 		CONS(HEADER[3]=='3');
 		CONS(N==K);
 	}
 	
 	void Subtask4() {	//Bruteforce
-		Points(16);
+		Points(15);
 		CONS(HEADER[4]=='4');
 		CONS(Between(N,1,10));
 	}
 	
 	void Subtask5() {	//Cuma ada 1 garis
-		Points(10);
+		Points(23);
 		CONS(HEADER[5]=='5');
 		CONS(K==1);
 	}
 	
 	void Subtask6() {	//Tidak ada garis lurus
-		Points(19);
+		Points(18);
 		CONS(HEADER[6]=='6');
 		CONS(NoStraightLine(A,P));
 		CONS(NoStraightLine(B,P));
 	}
 	
 	void Subtask7() {	//Full
-		Points(30);
+		Points(20);
 		CONS(HEADER[7]=='7');
 	}
 private:
@@ -196,19 +196,29 @@ protected:
 		Subtasks({3,4,6,7}); string h="...34.67";
 		CASE(HEADER=h,N=K=2,P={1,2},A={2,1},B={2,1});
 		
-		for (int i=0;i<2;i++) {
+		for (int i=0;i<3;i++) {
 			CASE(HEADER=h, N=K=rnd.nextInt(8,10),randomP(P,K,N),randomAB(A,B,P,N,K,false));
 			CASE(HEADER=h, N=K=10,randomP(P,K,N),randomAB(A,B,P,N,K,false));
 		}
 	}
 	
-	void TestGroup5() {	//K=N, N<=200k, gak ada garis lurus
+	void TestGroup5() {	//K=N, N<=100k, gak ada garis lurus
 		Subtasks({3,6,7}); string h="...3..67";
 		
-		for (int i=0;i<2;i++) {
-			CASE(HEADER=h, N=K=rnd.nextInt(11,200000),randomP(P,K,N),randomAB(A,B,P,N,K,false));
-			CASE(HEADER=h, N=K=rnd.nextInt(100000,200000),randomP(P,K,N),randomAB(A,B,P,N,K,false));
-			CASE(HEADER=h, N=K=200000,randomP(P,K,N),randomAB(A,B,P,N,K,false));
+		//10<N<=10000
+		{
+			int MIN_N=11, MAX_N=10000;
+			CASE(HEADER=h, N=K=rnd.nextInt(MIN_N,MIN_N+100),randomP(P,K,N),randomAB(A,B,P,N,K,false));
+			CASE(HEADER=h, N=K=rnd.nextInt(MIN_N,MAX_N),randomP(P,K,N),randomAB(A,B,P,N,K,false));
+			CASE(HEADER=h, N=K=rnd.nextInt(MAX_N-100,MAX_N),randomP(P,K,N),randomAB(A,B,P,N,K,false));
+		}
+		
+		//10000<N<=100k
+		{
+			int MIN_N=10001, MAX_N=100000;
+			CASE(HEADER=h, N=K=rnd.nextInt(MIN_N,MIN_N+100),randomP(P,K,N),randomAB(A,B,P,N,K,false));
+			CASE(HEADER=h, N=K=rnd.nextInt(MIN_N,MAX_N),randomP(P,K,N),randomAB(A,B,P,N,K,false));
+			CASE(HEADER=h, N=K=rnd.nextInt(MAX_N-100,MAX_N),randomP(P,K,N),randomAB(A,B,P,N,K,false));
 		}
 	}
 	
@@ -222,21 +232,31 @@ protected:
 		CASE(HEADER=h,N=K=rnd.nextInt(3,10),blocksAB(A,B,P,N,K));
 		CASE(HEADER=h,N=K=10,blocksAB(A,B,P,N,K));
 		
-		for (int i=0;i<2;i++) {
+		for (int i=0;i<3;i++) {
 			CASE(HEADER=h,N=K=rnd.nextInt(8,10),randomP(P,K,N),randomAB(A,B,P,N,K));
 			CASE(HEADER=h,N=K=10,randomP(P,K,N),randomAB(A,B,P,N,K));
 		}
 	}
 	
-	void TestGroup7() {	//K=N, N<=200k
+	void TestGroup7() {	//K=N, N<=100k
 		Subtasks({3,7}); string h="...3...7";
 		
-		CASE(HEADER=h,N=K=rnd.nextInt(3,200000),blocksAB(A,B,P,N,K));
-		CASE(HEADER=h,N=K=200000,blocksAB(A,B,P,N,K));
+		//10<N<=10000
+		{
+			int MIN_N=11, MAX_N=10000;
+			CASE(HEADER=h,N=K=rnd.nextInt(MIN_N,MIN_N+100),blocksAB(A,B,P,N,K));
+			CASE(HEADER=h,N=K=rnd.nextInt(MIN_N,MAX_N),blocksAB(A,B,P,N,K));
+			CASE(HEADER=h,N=K=rnd.nextInt(MAX_N-100,MAX_N),blocksAB(A,B,P,N,K));
+			CASE(HEADER=h,N=K=rnd.nextInt(MIN_N,MAX_N),randomP(P,K,N),randomAB(A,B,P,N,K));
+		}
 		
-		for (int i=0;i<2;i++) {
-			CASE(HEADER=h,N=K=rnd.nextInt(3,200000),randomP(P,K,N),randomAB(A,B,P,N,K));
-			CASE(HEADER=h,N=K=200000,randomP(P,K,N),randomAB(A,B,P,N,K));
+		//10000<N<=100000
+		{
+			int MIN_N=10001, MAX_N=100000;
+			CASE(HEADER=h,N=K=rnd.nextInt(MIN_N,MIN_N+100),blocksAB(A,B,P,N,K));
+			CASE(HEADER=h,N=K=rnd.nextInt(MIN_N,MAX_N),blocksAB(A,B,P,N,K));
+			CASE(HEADER=h,N=K=rnd.nextInt(MAX_N-100,MAX_N),blocksAB(A,B,P,N,K));
+			CASE(HEADER=h,N=K=rnd.nextInt(MIN_N,MAX_N),randomP(P,K,N),randomAB(A,B,P,N,K));
 		}
 	}
 	
@@ -251,7 +271,7 @@ protected:
 			CASE(HEADER=h,N=temp+1,K=1,randomP(P,K,N),randomAB(A,B,P,N,K));
 		}
 		
-		for (int i=0;i<2;i++) {
+		for (int i=0;i<3;i++) {
 			int temp=rnd.nextInt(3,9);
 			CASE(HEADER=h,N=temp,K=1,blocksAB(A,B,P,N,K));
 			CASE(HEADER=h,N=temp+1,K=1,blocksAB(A,B,P,N,K));
@@ -264,24 +284,13 @@ protected:
 		
 	}
 	
-	void TestGroup9() {	//K=1, N<=200k
+	void TestGroup9() {	//K=1, N<=100k
 		Subtasks({5,7}); string h=".....5.7";
-		for (int i=0;i<2;i++) {
-			int temp=rnd.nextInt(3,199999);
-			CASE(HEADER=h,N=temp,K=1,randomP(P,K,N),randomAB(A,B,P,N,K));
-			CASE(HEADER=h,N=temp+1,K=1,randomP(P,K,N),randomAB(A,B,P,N,K));
-		}
+		//10<N<=10000
+		generateTestGroup9(h,11,10000);
 		
-		for (int i=0;i<2;i++) {
-			int temp=rnd.nextInt(3,199999);
-			CASE(HEADER=h,N=temp,K=1,blocksAB(A,B,P,N,K));
-			CASE(HEADER=h,N=temp+1,K=1,blocksAB(A,B,P,N,K));
-		}
-		
-		CASE(HEADER=h,N=199999,K=1,randomP(P,K,N),randomAB(A,B,P,N,K));
-		CASE(HEADER=h,N=199999,K=1,blocksAB(A,B,P,N,K));
-		CASE(HEADER=h,N=200000,K=1,randomP(P,K,N),randomAB(A,B,P,N,K));
-		CASE(HEADER=h,N=200000,K=1,blocksAB(A,B,P,N,K));
+		//10000<N<=100k
+		generateTestGroup9(h,10001,100000);
 	}
 	
 	void TestGroup10() {	//Tidak ada garis lurus, N<=10
@@ -291,25 +300,32 @@ protected:
 		for (int i=0;i<5;i++) {
 			CASE(HEADER=h,N=10,generateAll(A,B,P,K,N,false));
 		}
-		for (int i=0;i<2;i++) {
+		for (int i=0;i<5;i++) {
 			CASE(HEADER=h,N=10,generateAll(A,B,P,K,N,false,false));
 		}
 		CASE(HEADER=h,N=10,K=rnd.nextInt(2,9),randomP(P,K,N),randomAB(A,B,P,N,K,false));
 		CASE(HEADER=h,N=10,K=rnd.nextInt(2,9),randomP(P,K,N),randomAB(A,B,P,N,K,false));
 	}
 	
-	void TestGroup11() {	//Tidak ada garis lurus, N<=200k
+	void TestGroup11() {	//Tidak ada garis lurus, N<=100k
 		Subtasks({6,7}); string h="......67";
-		for (int i=0;i<5;i++) {
-			CASE(HEADER=h,N=rnd.nextInt(10,200000),generateAll(A,B,P,K,N,false));
-			CASE(HEADER=h,N=200000,generateAll(A,B,P,K,N,false));
+		//10<N<=10000
+		{
+			int MIN_N=11, MAX_N=10000;
+			CASE(HEADER=h,N=rnd.nextInt(MIN_N,MAX_N),generateAll(A,B,P,K,N,false));
+			CASE(HEADER=h,N=rnd.nextInt(MAX_N-100,MAX_N),generateAll(A,B,P,K,N,false));
+			CASE(HEADER=h,N=rnd.nextInt(MIN_N,MAX_N),generateAll(A,B,P,K,N,false,false));
+			CASE(HEADER=h,N=rnd.nextInt(MAX_N-100,MAX_N),generateAll(A,B,P,K,N,false,false));
 		}
-		for (int i=0;i<2;i++) {
-			CASE(HEADER=h,N=rnd.nextInt(10,200000),generateAll(A,B,P,K,N,false,false));
-			CASE(HEADER=h,N=200000,generateAll(A,B,P,K,N,false,false));
+		
+		//10000<N<=100k
+		{
+			int MIN_N=10001, MAX_N=100000;
+			CASE(HEADER=h,N=rnd.nextInt(MIN_N,MAX_N),generateAll(A,B,P,K,N,false));
+			CASE(HEADER=h,N=rnd.nextInt(MAX_N-100,MAX_N),generateAll(A,B,P,K,N,false));
+			CASE(HEADER=h,N=rnd.nextInt(MIN_N,MAX_N),generateAll(A,B,P,K,N,false,false));
+			CASE(HEADER=h,N=rnd.nextInt(MAX_N-100,MAX_N),generateAll(A,B,P,K,N,false,false));
 		}
-		CASE(HEADER=h,N=rnd.nextInt(100000,200000),K=rnd.nextInt(2,N-1),randomP(P,K,N),randomAB(A,B,P,N,K,false));
-		CASE(HEADER=h,N=200000,K=rnd.nextInt(2,N-1),randomP(P,K,N),randomAB(A,B,P,N,K,false));
 	}
 	
 	void TestGroup12() {	//N<=10
@@ -329,46 +345,66 @@ protected:
 		CASE(HEADER=h,N=10,K=rnd.nextInt(2,N-1),randomP(P,K,N),randomAB(A,B,P,N,K));
 	}
 	
-	void TestGroup13() {	//N<=200k
+	void TestGroup13() {	//N<=100k
 		Subtasks({7}); string h=".......7";
-		for (int i=0;i<10;i++) {
-			CASE(HEADER=h,N=rnd.nextInt(11,200000),generateAll(A,B,P,K,N));
-			CASE(HEADER=h,N=200000,generateAll(A,B,P,K,N));
-		}
-		for (int i=0;i<10;i++) {
-			CASE(HEADER=h,N=rnd.nextInt(11,200000),K=rnd.nextInt(2,N),blocksAB(A,B,P,N,K));
-			CASE(HEADER=h,N=200000,K=rnd.nextInt(2,N-1),blocksAB(A,B,P,N,K));
-		}
-		//K<=5
-		for (int i=0;i<2;i++) {
-			CASE(HEADER=h,N=rnd.nextInt(11,200000),K=rnd.nextInt(2,min(5,N-1)),blocksAB(A,B,P,N,K));
-			CASE(HEADER=h,N=200000,K=5,blocksAB(A,B,P,N,K));
-		}
-		//K<=25
-		for (int i=0;i<2;i++) {
-			CASE(HEADER=h,N=rnd.nextInt(11,200000),K=rnd.nextInt(2,min(25,N-1)),blocksAB(A,B,P,N,K));
-			CASE(HEADER=h,N=200000,K=25,blocksAB(A,B,P,N,K));
-		}
-		//K<=625
-		for (int i=0;i<2;i++) {
-			CASE(HEADER=h,N=rnd.nextInt(11,200000),K=rnd.nextInt(2,min(625,N-1)),blocksAB(A,B,P,N,K));
-			CASE(HEADER=h,N=200000,K=625,blocksAB(A,B,P,N,K));
-		}
-		//K<=200k
-		for (int i=0;i<2;i++) {
-			CASE(HEADER=h,N=rnd.nextInt(11,200000),K=rnd.nextInt(2,N-1),blocksAB(A,B,P,N,K));
-			CASE(HEADER=h,N=200000,K=rnd.nextInt(100000,N-1),blocksAB(A,B,P,N,K));
-			CASE(HEADER=h,N=200000,K=100000,blocksAB(A,B,P,N,K));
-		}
-		//N>=100k
-		for (int i=0;i<2;i++) {
-			CASE(HEADER=h,N=rnd.nextInt(100000,200000),K=rnd.nextInt(2,N-1),blocksAB(A,B,P,N,K));
-			CASE(HEADER=h,N=rnd.nextInt(100000,200000),generateAll(A,B,P,K,N));
-		}
-		CASE(HEADER=h,N=rnd.nextInt(11,200000),K=rnd.nextInt(2,N-1),randomP(P,K,N),randomAB(A,B,P,N,K));
-		CASE(HEADER=h,N=200000,K=rnd.nextInt(2,N-1),randomP(P,K,N),randomAB(A,B,P,N,K));
+
+		//10<N<=100
+		generateTestGroup13(h,11,100);
+
+		//100<N<=1000
+		generateTestGroup13(h,101,1000);
+		
+		//1000<N<=10000
+		generateTestGroup13(h,1001,10000);
+		
+		//10000<N<=90k
+		generateTestGroup13(h,10001,90000);
+		
+		//90k<N<=100k
+		generateTestGroup13(h,90001,100000);
 	}
 private:
+	void generateTestGroup9(string h,int MIN_N,int MAX_N) {
+		//Random
+		generateTestGroup9Random(h,rnd.nextInt(MIN_N,MIN_N+100));
+		generateTestGroup9Random(h,rnd.nextInt(MIN_N,MAX_N-1));
+		generateTestGroup9Random(h,rnd.nextInt(MAX_N-100,MAX_N-1));
+		
+		//Blocks
+		generateTestGroup9Blocks(h,rnd.nextInt(MIN_N,MIN_N+100));
+		generateTestGroup9Blocks(h,rnd.nextInt(MIN_N,MAX_N-1));
+		generateTestGroup9Blocks(h,rnd.nextInt(MAX_N-100,MAX_N-1));
+	}
+	void generateTestGroup9Random(string h,int VAL_N) {
+		CASE(HEADER=h,N=VAL_N,K=1,randomP(P,K,N),randomAB(A,B,P,N,K));
+		CASE(HEADER=h,N=VAL_N+1,K=1,randomP(P,K,N),randomAB(A,B,P,N,K));
+	}
+	void generateTestGroup9Blocks(string h,int VAL_N) {
+		CASE(HEADER=h,N=VAL_N,K=1,blocksAB(A,B,P,N,K));
+		CASE(HEADER=h,N=VAL_N+1,K=1,blocksAB(A,B,P,N,K));
+	}
+	
+	void generateTestGroup13(string h,int MIN_N,int MAX_N) {
+		//All
+		CASE(HEADER=h,N=rnd.nextInt(MIN_N,MIN_N+100),generateAll(A,B,P,K,N));
+		CASE(HEADER=h,N=rnd.nextInt(MIN_N,MAX_N),generateAll(A,B,P,K,N));
+		CASE(HEADER=h,N=rnd.nextInt(MAX_N-100,MAX_N),generateAll(A,B,P,K,N));
+		
+		//Blocks, 1<K<MAX_N
+		generateTestGroup13Blocks(h,MIN_N,MAX_N,2,MAX_N-1);
+		
+		//Blocks, 10<K<MAX_N
+		generateTestGroup13Blocks(h,MIN_N,MAX_N,11,MAX_N-1);
+		
+		//Random
+		CASE(HEADER=h,N=rnd.nextInt(MAX_N-100,MAX_N),K=rnd.nextInt(2,N-1),randomP(P,K,N),randomAB(A,B,P,N,K));
+	}
+	void generateTestGroup13Blocks(string h,int MIN_N,int MAX_N,int MIN_K,int MAX_K) {
+		CASE(HEADER=h,N=rnd.nextInt(MIN_N,MIN_N+100),K=rnd.nextInt(MIN_K,min(MAX_K,N-1)),blocksAB(A,B,P,N,K));
+		CASE(HEADER=h,N=rnd.nextInt(MIN_N,MAX_N),K=rnd.nextInt(MIN_K,min(MAX_K,N-1)),blocksAB(A,B,P,N,K));
+		CASE(HEADER=h,N=rnd.nextInt(MAX_N-100,MAX_N),K=rnd.nextInt(MIN_K,min(MAX_K,N-1)),blocksAB(A,B,P,N,K));
+	}
+	
 	void randomP(vector<int> &V,int k,int n) {
 		V.clear();
 		vector<int> temp;
@@ -428,7 +464,7 @@ private:
 	}
 	void reindexing(vector<int> &P,vector<int> &A,vector<int> &B) {	//Zero based
 		vector<int> conv=P; rnd.shuffle(conv.begin(),conv.end());
-		int idxConv[200010]; for (int i=0;i<P.size();i++) idxConv[conv[i]]=i;
+		int idxConv[100010]; for (int i=0;i<P.size();i++) idxConv[conv[i]]=i;
 		int convAB[P.size()+10]; for (int i=0;i<P.size();i++) convAB[i]=idxConv[P[i]];
 		for (int i=0;i<A.size();i++) A[i]=convAB[A[i]], B[i]=convAB[B[i]];
 		P=conv;
@@ -485,35 +521,43 @@ private:
 	}
 	void generateAll(vector<int> &A,vector<int> &B,vector<int> &P,int &K,int n,bool garisLurus=true,bool solutionExists=true) {
 		//Subtask 6 (gak ada garis lurus), garisLurus=false
-		//Dijamin ada solusi (N>=10), solutionExists=true
-		A.clear(); B.clear(); P.clear();
-		for (int i=0;i<n;i++) A.push_back(-1), B.push_back(-1);
-		
-		set<int> tempP,tempB,setP; tempP.clear(); tempB.clear(); setP.clear();
-		int idxP[200010]; memset(idxP,-1,sizeof idxP);
-		for (int i=0;i<n;i++) tempP.insert(i), tempB.insert(i);
-		
-		for (int i=0;i<n;i++) {
-			auto it=tempB.end(); it--;
-			int p,b;
-			while (true) {
-				b=getRandomElementInSet(0,*it,tempB,false);
-				if (garisLurus) p=rnd.nextInt(0,n-1);
-				else p=rnd.nextInt(1,n-2);
-				if (tempP.size()==1 && tempP.count(p)) continue;
-				if (solutionExists && validAssign(i,p,b,garisLurus)) break;
-				if (!solutionExists && (garisLurus || (p!=i && p!=b))) break;
+		//Dijamin ada solusi (N>10), solutionExists=true
+		while (true) {
+			A.clear(); B.clear(); P.clear();
+			for (int i=0;i<n;i++) A.push_back(-1), B.push_back(-1);
+			int stuck=0;
+			
+			set<int> tempP,tempB,setP; tempP.clear(); tempB.clear(); setP.clear();
+			int idxP[100010]; memset(idxP,-1,sizeof idxP);
+			for (int i=0;i<n;i++) tempP.insert(i), tempB.insert(i);
+			
+			for (int i=0;i<n;i++) {
+				auto it=tempB.end(); it--;
+				int p,b;
+				while (true) {
+					b=getRandomElementInSet(0,*it,tempB,false);
+					if (garisLurus) p=rnd.nextInt(0,n-1);
+					else p=rnd.nextInt(1,n-2);
+					if (tempP.size()==1 && tempP.count(p)) continue;
+					if (solutionExists && validAssign(i,p,b,garisLurus)) break;
+					if (!solutionExists && (garisLurus || (p!=i && p!=b))) break;
+					if (stuck==10000 && n<=100) break;
+					stuck++;
+				}
+				if (stuck==10000 && n<=10) break;
+				if (setP.count(p)) {
+					A[i]=B[b]=idxP[p]; tempB.erase(b);
+				}
+				else {
+					A[i]=(int)P.size(); 
+					B[b]=(int)P.size();
+					idxP[p]=(int)P.size();
+					P.push_back(p); 
+					setP.insert(p); tempP.erase(p); tempB.erase(b);
+				}
 			}
-			if (setP.count(p)) {
-				A[i]=B[b]=idxP[p]; tempB.erase(b);
-			}
-			else {
-				A[i]=(int)P.size(); 
-				B[b]=(int)P.size();
-				idxP[p]=(int)P.size();
-				P.push_back(p); 
-				setP.insert(p); tempP.erase(p); tempB.erase(b);
-			}
+			if (stuck==10000 && n<=10) continue;
+			else break;
 		}
 		reindexing(P,A,B);
 		K=P.size(); 
