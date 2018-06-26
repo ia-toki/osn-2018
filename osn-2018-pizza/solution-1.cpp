a4ad35637bb548e36c586d07eec637be19d2520e
Muhammad Ayaz Dzulfikar <muhammad.ayaz97@gmail.com>
2018-06-26 08:11:43 +0700

    Add solution-1 for osn-2018-pizza

---
+++ osn-2018-pizza/solution-1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "4" << endl;
+  cout << "4" << endl;
+  cout << "4" << endl;
+  cout << "4" << endl;
+  cout << "4" << endl;
+  cout << "4" << endl;
+  cout << "3" << endl;
+  cout << "3" << endl;
+  cout << "3" << endl;
+  cout << "3" << endl;
+  cout << "3" << endl;
+  cout << "3" << endl;
+  cout << "0" << endl;
+  
+  return 0;
+}
