a0c755869a82ef354d00f75337bdee6a87f2c7dc
alijaya <alijayameilio@gmail.com>
2018-06-30 17:34:17 +0200

    reformat tc

--- src/views/Custom.vue
+++ src/views/Custom.vue
@@ -64,13 +64,16 @@ export default {
   },
   methods: {
     handleQuery (tc) {
+      this.playing = false
       tc = tc || ''
       this.form.tc = tc
       if (tc != '') {
         this.$refs.form.validate((valid) => {
           if (valid) {
-            this.playing = true
-            this.$refs.game.changeTC(this.form.tc)
+            this.reformat(this.form.tc).then((reformat) => {
+              this.playing = true
+              this.$refs.game.changeTC(reformat)
+            })
           }
         })
       }
@@ -82,66 +85,87 @@ export default {
         }
       })
     },
-    validateTC (rule, value, callback) {
-      const input = new PassThrough()
-      input.write(value)
-      const interpreter = new ServerInterpreter(input)
+    async reformat (value) {
+      const conf = await this.interpretConf(value)
+      return [
+        conf.subtask, 
+        `${conf.N} ${conf.S}`, 
+        conf.A.join(' '),
+        `${conf.D} ${conf.Didx.join(' ')}`,
+        `${conf.J} ${conf.Jidx.join(' ')}`,
+      ].join('\n') + '\n'
+    },
+    interpretConf (value) {
+      return new Promise((resolve, reject) => {
+        const input = new PassThrough()
+        input.write(value)
+        const interpreter = new ServerInterpreter(input)
 
-      const unique = (value, index, self) => self.indexOf(value) == index
+        let ret = null
 
-      let err = ['Format testcase tidak benar\n(Tips: jangan lupa newline di akhir baris)']
-      interpreter.init = (conf) => {
-        err = []
-        if (!(1 <= conf.N && conf.N <= 10)) {
-          err.push('1 <= N <= 10.')
+        const unique = (value, index, self) => self.indexOf(value) == index
+
+        let err = ['Format testcase tidak benar\n(Tips: jangan lupa newline di akhir baris)']
+        interpreter.init = (conf) => {
+          ret = conf
+          err = []
+          if (!(1 <= conf.N && conf.N <= 10)) {
+            err.push('1 <= N <= 10.')
+          }
+          if (!(conf.S == 1)) {
+            err.push('S = 1.')
+          }
+          conf.A.forEach((v, i) => {
+            if (!(1 <= v && v <= 10)) {
+              err.push(`1 <= A_${i+1} <= 10.`)
+            }
+          })
+          if (!(0 <= conf.D && conf.D <= conf.N)) {
+            err.push('0 <= P <= N.')
+          }
+          if (!(0 <= conf.J && conf.J <= conf.N)) {
+            err.push('0 <= Q <= N.')
+          }
+
+          conf.Didx.forEach((v, i) => {
+            if (!(1 <= v && v <= conf.N)) {
+              err.push(`1 <= X_${i+1} <= N.`)
+            }
+          })
+
+          if (!(conf.Didx.length == conf.Didx.filter(unique).length)) {
+            err.push('Pizza Anda harus unik.')
+          }
+
+          conf.Jidx.forEach((v, i) => {
+            if (!(1 <= v && v <= conf.N)) {
+              err.push(`1 <= Y_${i+1} <= N.`)
+            }
+          })
+
+          if (!(conf.Jidx.length == conf.Jidx.filter(unique).length)) {
+            err.push('Pizza Dengklek harus unik.')
+          }
         }
-        if (!(conf.S == 1)) {
-          err.push('S = 1.')
-        }
-        conf.A.forEach((v, i) => {
-          if (!(1 <= v && v <= 10)) {
-            err.push(`1 <= A_${i+1} <= 10.`)
+
+        interpreter.start().then(() => {
+          if (err.length === 0) {
+            resolve(ret)
+          } else {
+            reject(new Error(err.join('\n')))
           }
         })
-        if (!(0 <= conf.D && conf.D <= conf.N)) {
-          err.push('0 <= P <= N.')
-        }
-        if (!(0 <= conf.J && conf.J <= conf.N)) {
-          err.push('0 <= Q <= N.')
-        }
 
-        conf.Didx.forEach((v, i) => {
-          if (!(1 <= v && v <= conf.N)) {
-            err.push(`1 <= X_${i+1} <= N.`)
-          }
-        })
-
-        if (!(conf.Didx.length == conf.Didx.filter(unique).length)) {
-          err.push('Pizza Anda harus unik.')
-        }
-
-        conf.Jidx.forEach((v, i) => {
-          if (!(1 <= v && v <= conf.N)) {
-            err.push(`1 <= Y_${i+1} <= N.`)
-          }
-        })
-
-        if (!(conf.Jidx.length == conf.Jidx.filter(unique).length)) {
-          err.push('Pizza Dengklek harus unik.')
-        }
-
-      }
-
-      interpreter.start().then(() => {
-        if (err.length === 0) {
-          callback()
-        } else {
-          callback(new Error(err.join('\n')))
-        }
+        interpreter.break()
+        input.destroy()
       })
-
-      interpreter.break()
-      input.destroy()
+    },
+    validateTC (rule, value, callback) {
+      this.interpretConf(value).then(() => {
+        callback()
+      }, (e) => {
+        callback(e)
+      })
     }
   }
 }
